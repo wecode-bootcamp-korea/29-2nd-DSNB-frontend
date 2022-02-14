f12add33154f3e3e131f6c088712f623611e545b
moonkorea00 <moonkorea00@gmail.com>
2022-02-15 01:36:16 +0900

    추가 : footer - footer component - footer UI

--- src/Router.js
+++ src/Router.js
@@ -4,11 +4,13 @@ import Login from './pages/Login/Login';
 import ProductDetail from './pages/ProductDetail/ProductDetail';
 import ProductList from './pages/ProductList/ProductList';
 import MyLibrary from './pages/MyLibrary/MyLibrary';
+import Footer from './components/Footer/Footer';
 
 function Router() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/footer" element={<Footer />} />
         <Route path="/login" element={<Login />} />
         <Route path="/productDetail" element={<ProductDetail />} />
         <Route path="/productList" element={<ProductList />} />
